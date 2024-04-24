928ce46f16de1e8cd1fab21688d0fabd3418035f
jirkos.eu <146726029+jirkoseu@users.noreply.github.com>
2024-04-24 11:34:19 +0200

    jebingos

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -24,23 +24,37 @@ const SignIn : NextPage = () => {
 
     return (
         <>
-        <div className="flex h-full min-h-full justify-center items-center flex-col bg-gradient-to-b from-[#0D263D] to-[#141414] h-64">
+        <div className="flex h-full min-h-full justify-center items-center flex-col bg-gradient-to-b from-[#0D263D] to-[#141414]">
             <div className="flex h-min min-h-min justify-center items-center flex-col text-white p-[1px] bg-gradient-to-b from-[#EEBC8A] to-[#666666] h-64 rounded-xl">
-                <div className="flex h-min min-h-min justify-center items-center flex-row p-2 bg-gradient-to-b from-[#0D263D] to-[#141414] h-64 rounded-xl">
-                <div className="flex h-min min-h-min justify-center items-start flex-col bg-green-500">
-                        <h1>betton</h1>
-                        <div className="flex h-min min-h-min justify-center items-center flex-col p-2 bg-gradient-to-b from-[#FFD355] via-[#FF8D55] via-[#D33469] to-[#40348A] h-64 rounded-xl opacity-90">
-                        <p>Feel the thrill,</p>
-                        <p>place the bet,</p> 
-                        <p>take the victory lap!</p>
-                        <div className="flex h-min min-h-min justify-center items-center flex-col p-2 bg-white rounded">
+                <div className="flex h-min min-h-min justify-center items-center flex-row px-5 bg-gradient-to-b from-[#0D263D] to-[#141414] rounded-xl">
+                <div className="flex h-min min-h-min justify-center items-start flex-col">
+                        
+                        <p className="font-extrabold text-xl my-1">betton</p>
+                        <div className="text-xl flex min-h-min justify-center items-center flex-col p-2 mb-8 py-[85px] bg-gradient-to-b font-bold  from-[#FFD355] via-[#D33469] to-[#40348A] h-64 rounded-xl opacity-90">
+                        <p className="leading-tight text-[#FAECDE]">Feel the thrill,</p>
+                        <p className="leading-tight text-[#FAECDE]">place the bet,</p> 
+                        <p className="leading-tight text-[#FAECDE]">take the victory lap!</p>
+                    </div>
+                    </div>
+                    <div className="flex h-72 justify-center items-center flex-col p-[0.5px] mx-8 bg-slate-600 rounded">
                     
                     </div>
+                    <div className="flex h-min min-h-min justify-center items-center flex-col  rounded text-[#FAECDE]">
+                    <h1 className="text-xl font-bold mb-8">SIGN IN</h1>
+                    <div className="font-bold text-sm">
+                    <p>EMAIL</p>
+                    <div className="flex h-min min-h-min justify-center items-center flex-col text-white p-[1px] bg-gradient-to-b from-[#EEBC8A] to-[#666666] rounded-lg">
+                    <input className="text-[#FAECDE] bg-gradient-to-b from-[#0D263D] to-[#141414] rounded-lg h-6 w-48 px-2 focus:outline-[#666666] ease-in-out duration-300 text-sm indent-0.5 font-light" type="email"></input>
+                    </div>
+                    <p className="mt-2">PASSWORD</p>
+                    <div className="flex h-min min-h-min justify-center items-center flex-col text-white p-[1px] bg-gradient-to-b from-[#EEBC8A] to-[#666666] rounded-lg">
+                    <input className="text-[#FAECDE] bg-gradient-to-b from-[#0D263D] to-[#141414] rounded-lg h-6 w-48 px-2 focus:outline-[#666666] ease-in-out duration-300 text-sm indent-0.5 font-light" type="password"></input>
                     </div>
                     </div>
-                    <div className="flex h-min min-h-min justify-center items-center flex-col p-2 bg-white rounded">
+                    <button className=" bg-gradient-to-b from-[#FFC701] to-[#FF9900] mt-4 p-1 px-4 font-bold text-sm rounded-lg hover:rounded-xl ease-in-out duration-300 hover:text-black">SIGN IN</button>
+                    <Link className="font-bold text-xs mt-2 hover:text-[#FFC701] ease-in-out duration-300" href="/signup">SIGN UP</Link>
+                    </div>
                     
-                    </div>
                 </div>
             </div>
         </div>
