07b481001603a7cb2e0a8d76d12ad76d1f3574a5
jirkos.eu <146726029+jirkoseu@users.noreply.github.com>
2024-04-25 08:41:27 +0200

    jebingos jebingos

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -36,10 +36,12 @@ const SignIn : NextPage = () => {
                 <div className="flex h-min min-h-min justify-center items-center flex-row px-5 bg-gradient-to-b from-[#3A425A] to-[#0D263D] rounded-xl">
                 <div className="flex h-min min-h-min justify-center items-start flex-col bg-no-repeat bg-contain bg-center " style={{backgroundImage: `url(https://files.oaiusercontent.com/file-nuNg8AWXL4DnLoWCfgvT0r8p?se=2024-04-24T21%3A13%3A27Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dd7f97d29-aef6-4947-995a-e153f267aac9.webp&sig=zANYeHLvJ8pD%2BsNQWqOHov4IilBbpdfTuRG2d8Nvjbo%3D)`}}>
                         <p className="font-extrabold text-xl my-1">betton</p>
-                        <div className="w-52 text-xl flex min-h-min justify-center items-center flex-col p-2 mb-8 py-[85px] bg-gradient-to-b font-bold  from-[#FFD355] via-[#D33469] to-[#40348A] h-64 rounded-xl opacity-90">
+                        <div className="w-52 text-xl flex min-h-min justify-center items-center flex-col p-2 z-10 mb-8 py-[85px] bg-cover bg-center h-64 rounded-xl " style={{backgroundImage: `url(./images/loginbg.webp)`}}>
+                                <div className="flex h-64 min-h-64 justify-center items-center  w-52 rounded-xl flex-col bg-gradient-to-b font-bold  from-[#FFD355] via-[#D33469] to-[#40348A] opacity-90">
                         <p className="leading-tight text-[#FAECDE] animate-[fadeIn_0.3s_ease-in-out]">Feel the thrill,</p>
                         <p className="leading-tight text-[#FAECDE] animate-[fadeIn_0.3s_ease-in-out]">place the bet,</p> 
                         <p className="leading-tight text-[#FAECDE] animate-[fadeIn_0.3s_ease-in-out]">take the victory lap!</p>
+                        </div>
                     </div>
                     </div>
                     <div className="flex h-72 justify-center items-center flex-col p-[0.5px] mx-8 bg-slate-600 rounded">
